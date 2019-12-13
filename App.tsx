4c881064595e864ac162780361f8a7121001c6e7
Thomas <bbkings2002@gmail.com>
2019-12-13 09:25:33 +0800

    修正NativeModule是空物件問題

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RNToast from 'react-native-rn-toast';
+import * as ExpoToast from 'expo-toast';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component{
+  async componentDidMount(){
+    RNToast.show("Hello RN");
+    await ExpoToast.toastAsync({message:'Hello Expo'});
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
